df03b8124dbea27b41ca634a94bb47686361dc63
Kyle Gann <kylegann@gmail.com>
2013-01-18 01:20:25 -0500

    getting ready for clojars release

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject ring-audit-middleware "1.0.0"
+(defproject ring-audit-middleware "0.1.0"
   :description "Basic request auditing middleware for ring based applications."
+  :url "https://github.com/kgann/ring-audit-middleware"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clout "1.1.0"]]
   :dev-dependencies [[ring-mock "0.1.3"]])
